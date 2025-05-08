c8e1c01d7b3060ec91ef5c9a52b92549e886a7dd
Jauris Mousa <147750477+Jouriiis@users.noreply.github.com>
2025-05-08 12:42:45 -0700

    Move and started Game

--- app/src/main/java/com/example/chessappcst338/Game.java
+++ app/src/main/java/com/example/chessappcst338/Game.java
@@ -1,4 +1,25 @@
 package com.example.chessappcst338;
 
+import java.util.Stack;
+
 public class Game {
+    private Board board;
+    private Stack<Move> history;
+    private boolean isWhiteTurn;
+
+    public Game(){
+//        board = new Board()
+        history = new Stack<>();
+        isWhiteTurn = true;
+    }
+
+    public void play(){}
+
+    public void undo(){}
+
+    public void display(){}
+
+    public Move getLast(){
+        return history.peek();
+    }
 }
--- app/src/main/java/com/example/chessappcst338/Move.java
+++ app/src/main/java/com/example/chessappcst338/Move.java
@@ -1,4 +1,25 @@
 package com.example.chessappcst338;
 
 public class Move {
+    private int x;
+    private int y;
+    private Piece piece;
+
+    public Move(int x, int y, Piece piece){
+        this.x = x;
+        this.y = y;
+        this.piece = piece;
+    }
+
+    public getX() {
+        return x;
+    }
+
+    public getY() {
+        return y;
+    }
+
+    public getPiece() {
+        return piece;
+    }
 }
