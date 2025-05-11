e7f7c305a51920c9f23f3bdaab61e0b56952a4fb
Mindr0ide <142521261+Mindr0ide@users.noreply.github.com>
2025-05-11 16:19:43 -0700

    Update Game.java

--- app/src/main/java/com/example/chessappcst338/Game.java
+++ app/src/main/java/com/example/chessappcst338/Game.java
@@ -14,35 +14,35 @@ public class Game {
         isWhiteTurn = true;
     }
 
-    public void play(){
-        board.reset();
-        while(true){
-            this.display();
-            Move move;   //TODO  = display.getMove(); ici c'est l'entroid ou on get le move de l'utilisateur
-            if( isWhiteTurn && move.getPiece().getColor().equals("white") || // if the move is from the wright color
-                !isWhiteTurn && move.getPiece().getColor().equals("black") ){
-
-                if(move.getPiece().getPossibleMoves(move.getX(), move.getY(), board).contains(move)){ // if the move is valid
-
-                    history.push(move);
-                    board.setPieceAt(move.getX(), move.getY(), move.getPiece());
-                    isWhiteTurn = !isWhiteTurn;
-
-                }
-                else{
-                    System.out.println("Invalid move!");
-                    continue;
-                }
-
-            } else {
-                System.out.println("It's not your turn!");
-            }
-            
-            if(isCheckmate(board)){ // c'est une fct qui existe pas encore mais qui va check si le roi est en echec et mat
-                System.out.println("Checkmate! " + (isWhiteTurn ? "Black" : "White") + " wins!");
-                break;
-            }
-        }
+    public void play(Move move){
+        board.setPieceAt(move.getX(), move.getY(), move.getPiece());
+//        board.reset();
+//        while(true){
+//            this.display();
+//            //Move move;   //  = display.getMove(); ici c'est l'entroid ou on get le move de l'utilisateur
+//            if( isWhiteTurn && move.getPiece().getColor().equals("white") || // if the move is from the wright color
+//                !isWhiteTurn && move.getPiece().getColor().equals("black") ){
+//
+//                if(move.getPiece().possibleMoves(move.getX(), move.getY(), board).contains(move)){ // if the move is valid
+//
+//                    history.push(move);
+//                    board.setPieceAt(move.getX(), move.getY(), move.getPiece());
+//                    isWhiteTurn = !isWhiteTurn;
+//                }
+//                else{
+//                    System.out.println("Invalid move!");
+//                    continue;
+//                }
+//
+//            } else {
+//                System.out.println("It's not your turn!");
+//            }
+//
+//            if(isCheckmate(board)){ // c'est une fct qui existe pas encore mais qui va check si le roi est en echec et mat
+//                System.out.println("Checkmate! " + (isWhiteTurn ? "Black" : "White") + " wins!");
+//                break;
+//            }
+//        }
     }
 
     public void undo(){
