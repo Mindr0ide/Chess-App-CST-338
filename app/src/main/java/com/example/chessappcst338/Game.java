fd119de88c5bdcfc0afa26693e192976c60ffc61
Mindr0ide <142521261+Mindr0ide@users.noreply.github.com>
2025-05-10 20:07:42 -0700

    added some code for the game class

--- app/src/main/java/com/example/chessappcst338/Game.java
+++ app/src/main/java/com/example/chessappcst338/Game.java
@@ -8,18 +8,61 @@ public class Game {
     private boolean isWhiteTurn;
 
     public Game(){
-//        board = new Board()
+        board = new Board();
+        //board.reset();
         history = new Stack<>();
         isWhiteTurn = true;
     }
 
-    public void play(){}
+    public void play(){
+        board.reset();
+        while(true){
+            this.display();
+            Move move;   //TODO  = display.getMove(); ici c'est l'entroid ou on get le move de l'utilisateur
+            if( isWhiteTurn && move.getPiece().getColor().equals("white") || // if the move is from the wright color
+                !isWhiteTurn && move.getPiece().getColor().equals("black") ){
 
-    public void undo(){
+                if(move.getPiece().getPossibleMoves(move.getX(), move.getY(), board).contains(move)){ // if the move is valid
 
+                    history.push(move);
+                    board.setPieceAt(move.getX(), move.getY(), move.getPiece());
+                    isWhiteTurn = !isWhiteTurn;
+
+                }
+                else{
+                    System.out.println("Invalid move!");
+                    continue;
+                }
+
+            } else {
+                System.out.println("It's not your turn!");
+            }
+            
+            if(isCheckmate(board)){ // c'est une fct qui existe pas encore mais qui va check si le roi est en echec et mat
+                System.out.println("Checkmate! " + (isWhiteTurn ? "Black" : "White") + " wins!");
+                break;
+            }
+        }
     }
 
-    public void display(){}
+    public void undo(){
+        if(!history.isEmpty()){
+            Move lastMove = history.pop();
+            // TODO: undo the move on the board
+
+
+
+
+
+            isWhiteTurn = !isWhiteTurn;
+        } else {
+            System.out.println("No moves to undo!");
+        }
+    }
+
+    public void display(){
+        
+    }
 
     public Move getLast(){
         return history.pop();
